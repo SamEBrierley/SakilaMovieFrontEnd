be5e8b87f3c2ea4a32d6128b9dc7445ea1ff3c8d
SamEBrierley <samellisbrierley@gmail.com>
2022-02-25 12:47:57 +0000

    Uploaded current state of the Sakila front end with temporarty table data incorperated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,149 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  
+  class FilmRow extends React.Component {
+    render() {
+      const film = this.props.film;
+      const filmtitle = film.filmtitle
+
+  
+      return (
+        <tr>
+          <td>{filmtitle}</td>
+          <td>{film.price}</td>
+          <td>{film.rating}</td>
+          <td>{film.length}</td>
+          <td>{film.specialFeatures}</td>
+          <td>{film.rentalDuration}</td>
+        </tr>
+      );
+    }
+  }
+
+class FilmTable extends React.Component {
+    render() {
+        const filterText = this.props.filterText;
+        const inStockOnly = this.props.inStockOnly;
+        const rows = [];
+
+      
+        this.props.films.forEach((film) => {
+            if (film.filmtitle.indexOf(filterText) === -1) {
+              return;
+            }
+            if (inStockOnly && !film.stocked) {
+              return;
+            }
+
+        rows.push(
+          <FilmRow
+            film={film}
+            key={film.filmtitle} />
+        );
+      });
+  
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Rental Price </th>
+              <th>Rating</th>
+              <th>Length (Minutes)</th>
+              <th>Special Features</th>
+              <th>Rental Duration (Weeks)</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      );
+    }
+  }
+
+  class FilterableFilmTable extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        filterText: '',
+        inStockOnly: false
+      };
+      
+      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+      this.handleInStockChange = this.handleInStockChange.bind(this);
+    }
+  
+    handleFilterTextChange(filterText) {
+      this.setState({
+        filterText: filterText
+      });
+    }
+    
+    handleInStockChange(inStockOnly) {
+      this.setState({
+        inStockOnly: inStockOnly
+      })
+    }
+  
+    render() {
+      return (
+        <div>
+          <SearchBar
+            filterText={this.state.filterText}
+            inStockOnly={this.state.inStockOnly}
+            onFilterTextChange={this.handleFilterTextChange}
+            onInStockChange={this.handleInStockChange}
+          />
+          <FilmTable
+            films={this.props.films}
+            filterText={this.state.filterText}
+            inStockOnly={this.state.inStockOnly}
+          />
+        </div>
+      );
+    }
+  }
+
+
+  
+  class SearchBar extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+      this.handleInStockChange = this.handleInStockChange.bind(this);
+    }
+    
+    handleFilterTextChange(e) {
+      this.props.onFilterTextChange(e.target.value);
+    }
+    
+    handleInStockChange(e) {
+      this.props.onInStockChange(e.target.checked);
+    }
+    
+    render() {
+      return (
+        <form>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={this.props.filterText}
+            onChange={this.handleFilterTextChange}
+          />
+        </form>
+      );
+    }
+  }
+
+const FILMS = [
+    {filmtitle: 'ACADEMIC DINOSAUR', price: '$0.99', rating:'PG', length:86, specialFeatures:'Deleted Scenes, Behind the Scenes', rentalDuration:6},
+    {filmtitle: 'ADAPTATION HOLES', price: '$2.99', rating:'NC-17',length:50, specialFeatures:'Trailers, Deleted Scenes', rentalDuration:7},
+    {filmtitle: 'ACONDA CONFESSIONS', price: '$0.99', rating:'R', length:92, specialFeatures:'Trailers, Deleted Scenes', rentalDuration:3}
+  ];
+
+  ReactDOM.render(
+    <FilterableFilmTable films={FILMS} />,
+    document.getElementById('root')
+  );
