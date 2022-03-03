26f20289cb4e8259d63e2d88a670c5df1c85f8f7
SamEBrierley <samellisbrierley@gmail.com>
2022-03-03 11:23:10 +0000

    Linked the database up with the front end

--- src/index.js
+++ src/index.js
@@ -1,149 +1,136 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import axios from "axios";
+import bootstrap, { Dropdown } from "bootstrap";
 
+class FilmRow extends React.Component {
+  render() {
+    const filmData = this.props.filmInfo;
+    return (
+      <div id="filmRow">
+        <div>
+          <h3 id="filmTitle">{filmData.title}</h3>
+          <table id="filmData">
+            <thead>
+              <tr>
+                <th>Release Year</th>
+                <th>Length</th>
+                <th>Rating</th>
+                <th>Genre</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{filmData.release_year}</td>
+                <td>{filmData.length}</td>
+                <td>{filmData.rating}</td>
 
-  
-  class FilmRow extends React.Component {
-    render() {
-      const film = this.props.film;
-      const filmtitle = film.filmtitle
-
-  
-      return (
-        <tr>
-          <td>{filmtitle}</td>
-          <td>{film.price}</td>
-          <td>{film.rating}</td>
-          <td>{film.length}</td>
-          <td>{film.specialFeatures}</td>
-          <td>{film.rentalDuration}</td>
-        </tr>
-      );
-    }
+              </tr>
+            </tbody>
+          </table>
+          <p className="filmDataBottom">Description: {filmData.description}</p>
+        </div>
+        <div>
+          <p>
+            Film ID: {filmData.film_id}
+            <br />
+          </p>
+          <p></p>
+          <p></p>
+          <br />
+          <br />
+          <br />
+        </div>
+      </div>
+    );
   }
+}
+
+
 
 class FilmTable extends React.Component {
-    render() {
-        const filterText = this.props.filterText;
-        const inStockOnly = this.props.inStockOnly;
-        const rows = [];
-
-      
-        this.props.films.forEach((film) => {
-            if (film.filmtitle.indexOf(filterText) === -1) {
-              return;
-            }
-            if (inStockOnly && !film.stocked) {
-              return;
-            }
-
-        rows.push(
-          <FilmRow
-            film={film}
-            key={film.filmtitle} />
-        );
-      });
-  
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Rental Price </th>
-              <th>Rating</th>
-              <th>Length (Minutes)</th>
-              <th>Special Features</th>
-              <th>Rental Duration (Weeks)</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { films: [] };
   }
 
-  class FilterableFilmTable extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        filterText: '',
-        inStockOnly: false
-      };
-      
-      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-      this.handleInStockChange = this.handleInStockChange.bind(this);
-    }
-  
-    handleFilterTextChange(filterText) {
-      this.setState({
-        filterText: filterText
-      });
-    }
-    
-    handleInStockChange(inStockOnly) {
-      this.setState({
-        inStockOnly: inStockOnly
-      })
-    }
-  
-    render() {
-      return (
-        <div>
+  componentDidMount() {
+    axios
+      .get("http://54.198.232.90:8080/Home/AllFilms")
+      .then((response) => this.setState({ films: response.data }));
+  }
+
+  render() {
+    const film = this.state.films;
+    const filterText = this.props.filterText.toLowerCase();
+
+    const rows = [];
+
+    this.state.films.forEach((film) => {
+      if (film.title.toLowerCase().indexOf(filterText) === -1) {
+        return;
+      }
+      rows.push(<FilmRow filmInfo={film} key={film.title} />);
+    });
+
+    return (
+      <div>
+        <div>{rows}</div>
+      </div>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+    return (
+      <form className="SearchBar">
+        <input
+          type="text"
+          placeholder="Search Film...."
+          value={filterText}
+          onChange={(e) => this.props.onFilterTextChange(e.target.value)}
+        />
+      </form>
+    );
+  }
+}
+
+class FilmWikiHomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: "",
+    };
+
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+  }
+
+  handleFilterTextChange(FT) {
+    this.setState({
+      filterText: FT,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div></div>
+        <div className="FilmWikiHomePageCenter">
           <SearchBar
             filterText={this.state.filterText}
-            inStockOnly={this.state.inStockOnly}
             onFilterTextChange={this.handleFilterTextChange}
-            onInStockChange={this.handleInStockChange}
           />
           <FilmTable
             films={this.props.films}
             filterText={this.state.filterText}
-            inStockOnly={this.state.inStockOnly}
           />
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
 
-
-  
-  class SearchBar extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-      this.handleInStockChange = this.handleInStockChange.bind(this);
-    }
-    
-    handleFilterTextChange(e) {
-      this.props.onFilterTextChange(e.target.value);
-    }
-    
-    handleInStockChange(e) {
-      this.props.onInStockChange(e.target.checked);
-    }
-    
-    render() {
-      return (
-        <form>
-          <input
-            type="text"
-            placeholder="Search..."
-            value={this.props.filterText}
-            onChange={this.handleFilterTextChange}
-          />
-        </form>
-      );
-    }
-  }
-
-const FILMS = [
-    {filmtitle: 'ACADEMIC DINOSAUR', price: '$0.99', rating:'PG', length:86, specialFeatures:'Deleted Scenes, Behind the Scenes', rentalDuration:6},
-    {filmtitle: 'ADAPTATION HOLES', price: '$2.99', rating:'NC-17',length:50, specialFeatures:'Trailers, Deleted Scenes', rentalDuration:7},
-    {filmtitle: 'ACONDA CONFESSIONS', price: '$0.99', rating:'R', length:92, specialFeatures:'Trailers, Deleted Scenes', rentalDuration:3}
-  ];
-
-  ReactDOM.render(
-    <FilterableFilmTable films={FILMS} />,
-    document.getElementById('root')
-  );
+ReactDOM.render(<FilmWikiHomePage />, document.getElementById("root"));
