c8764b28c71f7acba7aaea9109fddd79a6e9580d
SamEBrierley <samellisbrierley@gmail.com>
2022-03-03 13:05:56 +0000

    Reinstalled axios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import axios from "axios";
+import axios from "./axios";
 
 class FilmRow extends React.Component {
   render() {
